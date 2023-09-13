edbec5c153e53faab5e817113c67cca819b51620
quang <nguyenthequang080203@gmail.com>
2023-09-13 10:28:45 +0700

    create sequelize migrations and models

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const sequelize = require("./services/database");
+const sequelize = require("./config/database");
 
 const app = express();
 const PORT = 3000;
---
+++ models/users/userModel.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const { Sequelize } = require("sequelize");
+const sequelize = require("../../config/database");
+
+let User = sequelize.define(
+  "users",
+  {
+    email: Sequelize.STRING,
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    role: {
+      type: Sequelize.ENUM("admin", "customer"),
+    },
+  },
+  {
+    tableName: "users",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    indexes: [
+      {
+        unique: true,
+        fields: ["id"],
+      },
+    ],
+    charset: "utf8",
+    collate: "utf8_unicode_ci",
+  }
+);
+
+module.exports = { User };
