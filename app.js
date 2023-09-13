d86afd7a27707e415accbcc079e57482b99aa911
quang <nguyenthequang080203@gmail.com>
2023-09-13 10:51:14 +0700

    config session and mysql session

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const sequelize = require("./config/database");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const expressValidator = require("express-validator");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const MySQLStore = require("express-mysql-session")(session);
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
+
+const options = {
+  host: process.env.MY_SQL_HOST,
+  port: PORT,
+  user: process.env.MY_SQL_USER,
+  password: process.env.MY_SQL_PASSWORD,
+  database: process.env.MY_SQL_DB,
+};
+const sessionStore = new MySQLStore(options);
+
+app.use(
+  session({
+    key: process.env.SESSION_KEY,
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 sequelize
   .authenticate()
